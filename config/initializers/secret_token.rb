98fe5c64f7a9c72cc70be789287ed115c6ffbaae
Jayson Isaac <jaysonissac@cronus-2.local>
2013-08-12 11:51:40 -0400

    changed

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,14 +1,12 @@
-
-require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
 
-# Set token based on intialize_secret function (defined in initializers/secret_generator.rb)
-RailsApp::Application.config.secret_token = initialize_secret(
-  :token,
-  '335a4e365ef2daeea969640d74e18f0e3cd9fae1abd8f4125691a880774ea6d456a29c0831aa6921bf86a710fe555e916f0673f5657619ec9df22e0409bec345'
-)
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awesomebook::Application.config.secret_key_base = 'edfefcafaeebc8d5a67ac6763b0ecf2900fa32f12d9f764d5eceebba188c9edb7d8f5ca5e85327d4e48466117270b14945c23d8bb259353597264b40824264b3'
